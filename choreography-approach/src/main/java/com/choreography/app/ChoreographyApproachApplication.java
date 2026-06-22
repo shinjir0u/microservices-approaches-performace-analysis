0c781133297c38d11215603b11b6e65f6d7ce73d
Htoo Aung Wai <htooagwai9@gmail.com>
2026-06-22 09:47:53 +0630

    Restructure project to setup folders for both apps

---
+++ choreography-approach/src/main/java/com/choreography/app/ChoreographyApproachApplication.java
@@ -0,0 +1,13 @@
+package com.choreography.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChoreographyApproachApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChoreographyApproachApplication.class, args);
+    }
+
+}
