a01f47029af74cc07f7918fd9f67253dfb9a08d3
Htoo Aung Wai <htooagwai9@gmail.com>
2026-06-22 10:08:53 +0630

    Finalize project structure for both approaches

---
+++ orchestration-approach/src/test/java/com/orchestration/app/OrchestrationApproachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orchestration.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrchestrationApproachApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
