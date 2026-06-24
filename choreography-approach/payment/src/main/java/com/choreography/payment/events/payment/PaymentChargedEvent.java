fc0b3db1fa1551bbbb59f5d05f400dbe3ef78bd4
Htoo Aung Wai <htooagwai9@gmail.com>
2026-06-24 16:53:27 +0630

    Implement payment charged event

--- choreography-approach/payment/src/main/java/com/choreography/payment/events/payment/PaymentChargedEvent.java
+++ choreography-approach/payment/src/main/java/com/choreography/payment/events/payment/PaymentChargedEvent.java
@@ -1,10 +1,14 @@
 package com.choreography.payment.events.payment;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
 
 import java.util.UUID;
 
+@Builder(toBuilder = true)
 public record PaymentChargedEvent(
+        @JsonProperty("event_id")
+        String eventId,
         @JsonProperty("payment_id")
         UUID paymentId,
         @JsonProperty("order_id")
--- choreography-approach/payment/src/main/java/com/choreography/payment/service/PaymentServiceImpl.java
+++ choreography-approach/payment/src/main/java/com/choreography/payment/service/PaymentServiceImpl.java
@@ -1,23 +1,46 @@
 package com.choreography.payment.service;
 
+import com.choreography.payment.events.payment.PaymentChargedEvent;
 import com.choreography.payment.model.Payment;
 import com.choreography.payment.repository.PaymentRepository;
-import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.time.Instant;
 import java.util.UUID;
 
+@Slf4j
 @Service
-@AllArgsConstructor
 public class PaymentServiceImpl implements PaymentService {
 
-    private final PaymentRepository paymentRepository;
+    @Autowired
+    private PaymentRepository paymentRepository;
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @Value("${order.rabbitmq.payment.charged.exchange}")
+    private String paymentChargedExchange;
+
+    @Value("${order.rabbitmq.payment.charged.routingKey}")
+    private String paymentChargedRoutingKey;
 
     @Override
     public Payment chargePayment(UUID orderId, BigDecimal amount) {
         var payment = Payment.builder().orderId(orderId).amount(amount).paidAt(Instant.now()).build();
+
+        var paymentChargedEvent = PaymentChargedEvent.builder()
+                        .eventId(UUID.randomUUID().toString())
+                        .paymentId(payment.getId())
+                        .orderId(payment.getOrderId())
+                        .build();
+
+        rabbitTemplate.convertAndSend(paymentChargedExchange, paymentChargedRoutingKey, paymentChargedEvent);
+        log.info("Created payment charged event with id: {}", paymentChargedEvent.);
         return paymentRepository.save(payment);
     }
 
