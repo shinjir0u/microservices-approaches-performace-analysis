f018f84d98f51aaa9c719297d87297726ef8cc01
Htoo Aung Wai <htooagwai9@gmail.com>
2026-06-26 13:36:23 +0630

    Add order listener for inventory reserved event

--- choreography-approach/order/src/main/java/com/choreography/order/listener/OrderListener.java
+++ choreography-approach/order/src/main/java/com/choreography/order/listener/OrderListener.java
@@ -1,5 +1,6 @@
 package com.choreography.order.listener;
 
+import com.choreography.order.events.inventory.InventoryReservedEvent;
 import com.choreography.order.events.payment.PaymentChargedEvent;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -12,6 +13,11 @@ public class OrderListener {
     @RabbitListener(queues = "${spring.rabbitmq.payment.charged.queue}")
     public void receivePayment(PaymentChargedEvent paymentChargedEvent) {
         log.info("Received paymentChargedEvent with id: {}", paymentChargedEvent.eventId());
+    }
+
+    @RabbitListener(queues = "${spring.rabbitmq.inventory.reserved.queue}")
+    public void receiveInventoryTransaction(InventoryReservedEvent inventoryReservedEvent) {
+        log.info("Received inventoryReservedEvent with id: {}", inventoryReservedEvent.eventId());
     }
 
 }
