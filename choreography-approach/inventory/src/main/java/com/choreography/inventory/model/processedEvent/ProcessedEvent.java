80407c01280351d0cf6a4dd8c1ff8f00e98430d9
Htoo Aung Wai <htooagwai9@gmail.com>
2026-06-29 14:12:36 +0630

    Enable builder for processedEvent for object creation

--- choreography-approach/inventory/src/main/java/com/choreography/inventory/model/processedEvent/ProcessedEvent.java
+++ choreography-approach/inventory/src/main/java/com/choreography/inventory/model/processedEvent/ProcessedEvent.java
@@ -2,13 +2,19 @@ package com.choreography.inventory.model.processedEvent;
 
 import com.choreography.inventory.model.processedEvent.type.EventStatus;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.Instant;
 import java.util.UUID;
 
 @Data
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
 @Table(name = "processed_events")
 public class ProcessedEvent {
 
--- choreography-approach/order/src/main/java/com/choreography/order/model/processedEvent/ProcessedEvent.java
+++ choreography-approach/order/src/main/java/com/choreography/order/model/processedEvent/ProcessedEvent.java
@@ -2,13 +2,19 @@ package com.choreography.order.model.processedEvent;
 
 import com.choreography.order.model.processedEvent.type.EventStatus;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.Instant;
 import java.util.UUID;
 
 @Data
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
 @Table(name = "processed_events")
 public class ProcessedEvent {
 
