3adb0af14bea4518e59420ebf365c5aae192cd82
Htoo Aung Wai <htooagwai9@gmail.com>
2026-07-13 16:47:15 +0630

    Add inventory compensation usecase on payment fail

--- choreography-approach/payment/src/main/java/com/choreography/payment/listener/PaymentListener.java
+++ choreography-approach/payment/src/main/java/com/choreography/payment/listener/PaymentListener.java
@@ -1,9 +1,11 @@
 package com.choreography.payment.listener;
 
+import com.choreography.payment.event.inventory.InventoryFailedEvent;
 import com.choreography.payment.event.order.OrderCreatedEvent;
 import com.choreography.payment.model.payment.Payment;
 import com.choreography.payment.service.rabbit.RabbitService;
 import com.choreography.payment.usecase.ChargePaymentUseCase;
+import com.choreography.payment.usecase.RevertPaymentUseCase;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -14,9 +16,11 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class PaymentListener {
 
+    private final RabbitService rabbitService;
+
     private final ChargePaymentUseCase chargePaymentUseCase;
 
-    private final RabbitService rabbitService;
+    private final RevertPaymentUseCase revertPaymentUseCase;
 
     @RabbitListener(queues = "${spring.rabbitmq.order.created.payment.queue}")
     public void receiveOrder(OrderCreatedEvent orderCreatedEvent) {
@@ -28,6 +32,13 @@ public class PaymentListener {
         } catch (IllegalArgumentException exception) {
             rabbitService.publishPaymentFailedEvent(orderCreatedEvent.orderId(), "Invalid amount.");
         }
+    }
+
+    @RabbitListener(queues = "${spring.rabbitmq.inventory.failed.payment.queue}")
+    public void revertPayment(InventoryFailedEvent inventoryFailedEvent) {
+        log.info("Received InventoryFailedEvent with id: {}", inventoryFailedEvent.eventId());
+
+        revertPaymentUseCase.execute(inventoryFailedEvent);
     }
 
 }
