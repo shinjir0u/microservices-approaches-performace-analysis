fa60de45827a685d535de9c6e7905f24da022fac
Htoo Aung Wai <htooaungwai@fedora.(none)>
2026-09-10 04:05:05 +0630

    Move rabbit service to message layer for separation of concerns

--- orchestration-approach/orchestrator/src/main/java/com/orchestration/orchestrator/listener/SagaStartListener.java
+++ orchestration-approach/orchestrator/src/main/java/com/orchestration/orchestrator/listener/SagaStartListener.java
@@ -1,7 +1,11 @@
 package com.orchestration.orchestrator.listener;
 
 import com.orchestration.orchestrator.config.RabbitMQSetting;
+import com.orchestration.orchestrator.model.SagaInstance;
+import com.orchestration.orchestrator.model.dto.InventoryCommand;
+import com.orchestration.orchestrator.model.dto.PaymentCommand;
 import com.orchestration.orchestrator.model.dto.SagaStartCommand;
+import com.orchestration.orchestrator.service.rabbit.RabbitService;
 import com.orchestration.orchestrator.usecase.StartSagaUseCase;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,10 +19,18 @@ public class SagaStartListener {
 
     private final StartSagaUseCase startSagaUseCase;
 
+    private final RabbitService rabbitService;
+
     @RabbitListener(queues = RabbitMQSetting.ORCHESTRATOR_START_QUEUE)
     public void receiveSagaStartCommand(SagaStartCommand sagaStartCommand) {
         log.info("Received SagaStartCommand with order id: {}", sagaStartCommand.orderId());
-        startSagaUseCase.execute(sagaStartCommand);
+        SagaInstance savedSagaInstance = startSagaUseCase.execute(sagaStartCommand);
+
+        InventoryCommand inventoryCommand = InventoryCommand.from(savedSagaInstance.getSagaId(), sagaStartCommand);
+        rabbitService.sendInventoryCommand(inventoryCommand);
+
+        PaymentCommand paymentCommand = PaymentCommand.from(savedSagaInstance.getSagaId(), sagaStartCommand);
+        rabbitService.sendPaymentCommand(paymentCommand);
     }
 
 }
--- orchestration-approach/orchestrator/src/main/java/com/orchestration/orchestrator/usecase/StartSagaUseCase.java
+++ orchestration-approach/orchestrator/src/main/java/com/orchestration/orchestrator/usecase/StartSagaUseCase.java
@@ -1,11 +1,8 @@
 package com.orchestration.orchestrator.usecase;
 
 import com.orchestration.orchestrator.model.SagaInstance;
-import com.orchestration.orchestrator.model.dto.InventoryCommand;
-import com.orchestration.orchestrator.model.dto.PaymentCommand;
 import com.orchestration.orchestrator.model.dto.SagaStartCommand;
 import com.orchestration.orchestrator.service.orchestrator.OrchestratorService;
-import com.orchestration.orchestrator.service.rabbit.RabbitService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -17,17 +14,9 @@ public class StartSagaUseCase {
 
     private final OrchestratorService orchestratorService;
 
-    private final RabbitService rabbitService;
-
-    public void execute(SagaStartCommand sagaStartCommand) {
+    public SagaInstance execute(SagaStartCommand sagaStartCommand) {
         SagaInstance sagaInstance = SagaInstance.from(sagaStartCommand);
-        SagaInstance savedSagaInstance = orchestratorService.save(sagaInstance);
-
-        InventoryCommand inventoryCommand = InventoryCommand.from(savedSagaInstance.getSagaId(), sagaStartCommand);
-        rabbitService.sendInventoryCommand(inventoryCommand);
-
-        PaymentCommand paymentCommand = PaymentCommand.from(savedSagaInstance.getSagaId(), sagaStartCommand);
-        rabbitService.sendPaymentCommand(paymentCommand);
+        return orchestratorService.save(sagaInstance);
     }
 
 }
