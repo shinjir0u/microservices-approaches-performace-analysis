eebcca503cbdebec6707d65dd17da0fb1e49fc63
Htoo Aung Wai <htooaungwai@fedora.(none)>
2026-09-10 04:26:29 +0630

    Remove unnecssary queue config

--- orchestration-approach/inventory/src/main/java/com/orchestration/inventory/config/RabbitMQConfiguration.java
+++ orchestration-approach/inventory/src/main/java/com/orchestration/inventory/config/RabbitMQConfiguration.java
@@ -11,21 +11,6 @@ import org.springframework.context.annotation.Configuration;
 public class RabbitMQConfiguration {
 
     @Bean
-    public Queue orchestratorStartQueue() {
-        return new Queue(RabbitMQSetting.ORCHESTRATOR_START_QUEUE);
-    }
-
-    @Bean
-    public Exchange orchestratorStartExchange() {
-        return new DirectExchange(RabbitMQSetting.ORCHESTRATOR_START_EXCHANGE);
-    }
-
-    @Bean
-    public Binding orchestratorStartBinding() {
-        return BindingBuilder.bind(orchestratorStartQueue()).to(orchestratorStartExchange()).with(RabbitMQSetting.ORCHESTRATOR_START_ROUTING_KEY).noargs();
-    }
-
-    @Bean
     public Queue orchestratorReplyQueue() {
         return new Queue(RabbitMQSetting.ORCHESTRATOR_REPLY_QUEUE);
     }
--- orchestration-approach/inventory/src/main/java/com/orchestration/inventory/config/RabbitMQSetting.java
+++ orchestration-approach/inventory/src/main/java/com/orchestration/inventory/config/RabbitMQSetting.java
@@ -2,10 +2,6 @@ package com.orchestration.inventory.config;
 
 public class RabbitMQSetting {
 
-    public static final String ORCHESTRATOR_START_QUEUE = "orchestrator.start.queue";
-    public static final String ORCHESTRATOR_START_EXCHANGE = "orchestrator.start.exchange";
-    public static final String ORCHESTRATOR_START_ROUTING_KEY = "orchestrator.start.routingkey";
-
     public static final String ORCHESTRATOR_REPLY_QUEUE = "orchestrator.reply.queue";
     public static final String ORCHESTRATOR_REPLY_EXCHANGE = "orchestrator.reply.exchange";
     public static final String ORCHESTRATOR_REPLY_ROUTING_KEY = "orchestrator.reply.routingkey";
