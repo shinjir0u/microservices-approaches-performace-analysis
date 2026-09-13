5aa379723994eacd7152d3e22e718e769f3947cf
Htoo Aung Wai <htooaungwai@fedora.(none)>
2026-09-13 17:07:58 +0630

    Setup order service for docker configuration

--- orchestration-approach/order/src/main/java/com/orchestration/order/config/RabbitMQSetting.java
+++ orchestration-approach/order/src/main/java/com/orchestration/order/config/RabbitMQSetting.java
@@ -2,16 +2,16 @@ package com.orchestration.order.config;
 
 public class RabbitMQSetting {
 
-    public static final String ORCHESTRATOR_START_QUEUE = "orchestrator.start.queue";
-    public static final String ORCHESTRATOR_START_EXCHANGE = "orchestrator.start.exchange";
-    public static final String ORCHESTRATOR_START_ROUTING_KEY = "orchestrator.start.routingkey";
+    public static final String ORCHESTRATOR_START_QUEUE = "${spring.rabbitmq.orchestrator.start.queue}";
+    public static final String ORCHESTRATOR_START_EXCHANGE = "${spring.rabbitmq.orchestrator.start.exchange}";
+    public static final String ORCHESTRATOR_START_ROUTING_KEY = "${spring.rabbitmq.orchestrator.start.routingKey}";
 
-    public static final String ORCHESTRATOR_ORDER_REPLY_QUEUE = "orchestrator.order.reply.queue";
-    public static final String ORCHESTRATOR_ORDER_REPLY_EXCHANGE = "orchestrator.order.reply.exchange";
-    public static final String ORCHESTRATOR_ORDER_REPLY_ROUTING_KEY = "orchestrator.order.reply.routingkey";
+    public static final String ORCHESTRATOR_ORDER_REPLY_QUEUE = "${spring.rabbitmq.orchestrator.reply.queue}";
+    public static final String ORCHESTRATOR_ORDER_REPLY_EXCHANGE = "${spring.rabbitmq.orchestrator.reply.exchange}";
+    public static final String ORCHESTRATOR_ORDER_REPLY_ROUTING_KEY = "${spring.rabbitmq.orchestrator.reply.routingKey}";
 
-    public static final String ORDER_COMMAND_QUEUE = "order.command.queue";
+    public static final String ORDER_COMMAND_QUEUE = "${spring.rabbitmq.order.command.queue}";
 
-    public static final String ORDER_FAIL_COMMAND_QUEUE = "order.fail.command.queue";
+    public static final String ORDER_FAIL_COMMAND_QUEUE = "${spring.rabbitmq.order.fail.command.queue}";
 
 }
